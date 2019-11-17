fe27d7a0f9edb9343b49c1c3741d9346fef0e2e9
Ankur Gupta <gupta.ankur255@gmail.com>
2019-11-17 18:31:47 +0530

    Added CHanges

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
+const http=require('http')
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const socketio=require('socket.io');
 
 const app=express();
+const server=http.createServer(app)
+const io=socketio(server)
+
 const port=process.env.PORT || 3000
 const publicPath=path.join(__dirname, '../public')
 
 app.use(express.json())
 app.use(express.static(publicPath))
 
-app.listen(port, ()=>{
+io.on('connection',(socket)=>{
+    console.log('New WebSocket Connection')
+
+    socket.emit('message', 'Welcome')
+    
+    socket.on('sendMessage', (message)=>{
+        io.emit('message', message)
+    })
+})
+
+server.listen(port, ()=>{
     console.log('Server is running on Port ' + port)
 })

2cddeb02478bb60e7457b9408cb5a2fdbe4bd158
Ankur Gupta <gupta.ankur255@gmail.com>
2020-01-09 20:32:29 +0530

    Added form and Button States

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,23 @@
 const socket = io()
 
+const messageForm=document.querySelector('#message-form')
+const messageFormInput=document.querySelector('input')
+const messageFormButton=document.querySelector('button')
+const locationButton=document.querySelector('#send-location')
+
 socket.on('message', (message)=>{
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e)=>{
+messageForm.addEventListener('submit', (e)=>{
     e.preventDefault()
-
-    const message=e.target.elements.message.value
+    messageFormButton.setAttribute('disabled','disabled')
+    const message = e.target.elements.message.value
     
     socket.emit('sendMessage', message, (error)=>{
+        messageFormButton.removeAttribute('disabled')
+        messageFormInput.value=''
+        messageFormInput.focus()
         if(error){
             return console.log(error)
         }
@@ -21,11 +29,13 @@ document.querySelector('#send-location').addEventListener('click', (e)=>{
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser')
     }
+    locationButton.setAttribute('disabled','disabled')
     navigator.geolocation.getCurrentPosition((position)=>{
         socket.emit('sendPosition', {
             Latitude: position.coords.latitude,
             Longitude: position.coords.longitude
         }, (message)=>{
+            locationButton.removeAttribute('disabled')
             console.log(message)
         })
     })
